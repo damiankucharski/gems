39d58efbe95371d5e45c93f9f7c81884fc5fdcdd
Damian Kucharski <s@Damians-MacBook-Pro.local>
2023-02-20 11:55:09 +0100

    Fix json save

--- io.py
+++ io.py
@@ -3,39 +3,38 @@ import json
 
 
 class Text:
-
     @staticmethod
     def load(path, lines=False):
-        with open(path, 'r') as file:
+        with open(path, "r") as file:
             if lines:
                 return file.readlines()
             return file.read()
 
     @staticmethod
     def save(path, string):
-        with open(path, 'w') as file:
+        with open(path, "w") as file:
             file.write(string)
+
 
 class Pickle:
     @staticmethod
     def load(path):
-        with open(path, 'rb') as file:
+        with open(path, "rb") as file:
             return pickle.load(file)
 
     @staticmethod
     def save(path, obj):
-        with open(path, 'wb') as file:
+        with open(path, "wb") as file:
             pickle.dump(obj, file)
 
 
 class Json:
-
     @staticmethod
     def load(path):
-        with open(path, 'r') as file:
+        with open(path, "r") as file:
             return json.load(file)
 
     @staticmethod
     def save(path, dictionary):
-        with open(path, 'wb') as file:
+        with open(path, "w") as file:
             json.dump(dictionary, file)
