4413fe2776a4d2de138a3cd5fc0bc3e9793a50d3
damiankucharski <c.damiankucharski@gmail.com>
2023-09-05 19:05:16 +0200

    add support for arrays in type

--- types/range.py
+++ types/range.py
@@ -7,13 +7,18 @@ class Range:
         self.right_inclusive = right_inclusive
 
     def __contains__(self, item):
-
-        if self.left_inclusive and self.left == item:
-            return True
-        if self.right_inclusive and self.right == item:
-            return True
-
-        return self.left < item < self.right
+        if hasattr(item, 'shape'):  # Check for array-like objects
+            left_condition = item >= self.left if self.left_inclusive else item > self.left
+            right_condition = item <= self.right if self.right_inclusive else item < self.right
+            return left_condition & right_condition
+        elif isinstance(item, list):
+            return [self.__contains__(i) for i in item]
+        else:
+            if self.left_inclusive and self.left == item:
+                return True
+            if self.right_inclusive and self.right == item:
+                return True
+            return self.left < item < self.right
 
 
 class OpenRange(Range):
